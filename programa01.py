1057c85cbd267a9694247027efd43ebc622795a2
Francisco López <39685222+franlop24@users.noreply.github.com>
2024-10-24 12:29:28 -0600

    Update programa01.py

--- programa01.py
+++ programa01.py
@@ -1,3 +1,4 @@
 #################################################################################
-#Programa que pida dos números e indique Cuál es el mayor
+# Programa que pida dos números e indique Cuál es el mayor
+# Si los dos son iguales que muestre el mensaje "Son iguales"
 #################################################################################
